aa6e6b04072135334857c388b1e6a7cf1f3b34c8
Igor Khokhlov <eld2303@gmail.com>
2013-03-21 16:06:00 +0200

    skeletone

---
+++ src/SilexSphinxsearch/SphinxsearchExtension.php
@@ -0,0 +1,27 @@
+<?php
+namespace SilexSphinxsearch;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+/**
+ * Sphinx search extension for Silex
+ */
+class SphinxsearchExtension extends ServiceProviderInterface
+{
+    /**
+     * @param \Silex\Application $app
+     */
+    public function boot(Application $app)
+    {
+
+    }
+
+    /**
+     * @param \Silex\Application $app
+     */
+    public function register(Application $app)
+    {
+
+    }
+}
